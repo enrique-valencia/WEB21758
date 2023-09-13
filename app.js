04f331101256713b9cd9cee632db4292aca5b15d
enrique-valencia <79728428+enrique-valencia@users.noreply.github.com>
2023-09-12 22:22:24 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+let x=10;
+let y=2;
+var resultado=x+y;
+console.log(resultado);
+
+//concatenacion
+var uno="hola";
+var dos="mundo";
+var joined= uno + dos;
+console.log(joined);
+
+var numeroUno=200;
+var numeroDos=100;
+console.log(numeroUno+numeroDos);
+
+
+//
+
+let nombre=prompt("Ingresa tu nombre");
+console.log(nombre)
+
+let apellido=prompt("Ingresa tu apellido");
+console.log(typeof apellido);
+console.log(apellido);
+
+
+let numero1 = prompt("Ingresa un número");
+let numero2 = prompt("Ingresa un segundo número");
+console.log(typeof numero1);
+console.log(typeof numero2);
+
+
+var resultado = numero1 + numero2;
+var resultado=parseInt(numero1)+parseInt(numero2);
+
+console.log(resultado);
+
+//SUMA
+let numeroU= prompt("Ingresa la primer cantidad");
+let numeroD = prompt("Imgresa la segunda cantidad");
+let resultado=parseInt(numeroU)+parseInt(numeroD);
+console.log(resultado);
+
+//RESTA
+let numeroU1= prompt("Ingresa la primer cantidad");
+let numeroD2 = prompt("Imgresa la segunda cantidad");
+let resultado=parseInt(numeroU1)-parseInt(numeroD2);
+console.log(resultado);
+
+//MULTIPLICACION
+let numeroUN1= prompt("Ingresa la primer cantidad");
+let numeroDO2 = prompt("Imgresa la segunda cantidad");
+let resultado=parseInt(numeroUN1)+parseInt(numeroDO2);
+console.log(resultado);
